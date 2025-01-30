23f4ee4e7258ce6a6c5bb65cf2ddcae4539c6303
12aptor <paholo2054@gmail.com>
2025-01-29 21:05:32 -0500

    introducción a flask

--- semana02/intro_flask/app.py
+++ semana02/intro_flask/app.py
@@ -1,5 +1,5 @@
 # Importación de la clase Flask
-from flask import Flask
+from flask import Flask, request
 
 # Instancia de la aplicación Flask
 app = Flask(__name__)
@@ -16,6 +16,36 @@ def send_json():
 @app.route('/html')
 def send_html():
     return '<button>Hola Flask 😎!</button>'
+
+# Recibe un parámetro de la ruta
+@app.route('/user/<username>')
+# @app.route('/user/<int:age>') # string, int, float, path, uuid
+def user(username):
+    return f'Hola {username}'
+
+@app.route('/users', methods=['GET', 'POST']) # DELETE, PUT, PATCH, OPTIONS
+def users():
+    method = request.method
+    if method == 'GET':
+        return [
+            {
+                'id': 1,
+                'name': 'John Doe'
+            },
+            {
+                'id': 2,
+                'name': 'Jane Doe'
+            }
+        ]
+    elif method == 'POST':
+        json = request.get_json()
+        return json
+    
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['image']
+    print(file)
+    return 'Uploaded!'
 
 # Ejecución de la aplicación Flask
 if __name__ == '__main__':
