9910f089cdcf508b59d49624c58d6b9ae7ed5689
12aptor <paholo2054@gmail.com>
2025-02-13 21:01:03 -0500

    categorias

--- semana03/flask-ecommerce/app/routes.py
+++ semana03/flask-ecommerce/app/routes.py
@@ -2,9 +2,12 @@ from flask_restful import Api
 from app import app
 from app.resources.role_resource import RoleResource
 from app.resources.auth_resource import RegisterResource, LoginResource
+from app.resources.category_resource import CategoryResource
 
 api = Api(app, prefix='/api')
 
 api.add_resource(RoleResource, '/roles')
 api.add_resource(RegisterResource, '/auth/register')
 api.add_resource(LoginResource, '/auth/login')
+
+api.add_resource(CategoryResource, '/categories')
