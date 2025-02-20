699ab60962a8c3927ed58fcc119f77db6b06c3f3
12aptor <paholo2054@gmail.com>
2025-02-19 20:03:09 -0500

    Actualizar productos

--- semana03/flask-ecommerce/app/resources/product_resource.py
+++ semana03/flask-ecommerce/app/resources/product_resource.py
@@ -125,3 +125,108 @@ class ProductResource(Resource):
             return {
                 'message': 'Unexpected error',
             }, 500
+
+
+class ProductManageResource(Resource):
+    def get(self, product_id):
+        try:
+            product = ProductModel.query.get(product_id)
+
+            if not product:
+                raise Exception('Product not found')
+            
+            response_data = ProductSchema(
+                id=product.id,
+                code=product.code,
+                name=product.name,
+                image=cloudinary.utils.cloudinary_url(product.image)[0],
+                description=product.description,
+                brand=product.brand,
+                size=product.size,
+                price=product.price,
+                stock=product.stock,
+                status=product.status,
+                created_at=str(product.created_at),
+                updated_at=str(product.updated_at),
+                category_id=product.category_id
+            )
+
+            return response_data.model_dump(), 200
+        except Exception as e:
+            return {
+                'message': 'Unexpected error',
+            }, 500
+
+    def put(self, product_id):
+        try:
+            product = ProductModel.query.get(product_id)
+
+            if not product:
+                raise Exception('Product not found')
+            
+            data = request.form
+            validated_data = CreateProductSchema(**data)
+
+            image = request.files.get('image')
+
+            if image is not None:
+                if not image.content_type.startswith('image/'):
+                    raise Exception('Invalida image format')
+            
+                file_size = image.read()
+                image.seek(0)
+                kb_size = len(file_size) / 1024
+                mb_size = kb_size / 1024
+
+                if mb_size > 2:
+                    raise Exception('Image size is too big')
+                
+                filename = image.filename.split('.')[0]
+                public_id = f'{uuid.uuid4()}-{filename}'
+
+                cloudinary.uploader.upload(
+                    file=image.stream,
+                    public_id=public_id
+                )
+                cloudinary.uploader.destroy(product.image)
+
+                product.image = product_id
+
+            product.name = validated_data.name
+            product.description = validated_data.description
+            product.brand = validated_data.brand
+            product.size = validated_data.size
+            product.price = validated_data.price
+            product.stock = validated_data.stock
+            product.category_id = validated_data.category_id
+
+            db.session.commit()
+
+            response_data = ProductSchema(
+                id=product.id,
+                code=product.code,
+                name=product.name,
+                image=cloudinary.utils.cloudinary_url(product.image)[0],
+                description=product.description,
+                brand=product.brand,
+                size=product.size,
+                price=product.price,
+                stock=product.stock,
+                status=product.status,
+                created_at=str(product.created_at),
+                updated_at=str(product.updated_at),
+                category_id=product.category_id
+            )
+            return response_data.model_dump(), 200
+        except ValidationError as e:
+            return {
+                'message': e.errors()
+            }, 400
+        except Exception as e:
+            db.session.rollback()
+            return {
+                'message': 'Unexpected error',
+            }, 500
+
+    def delete(self, product_id):
+        pass
