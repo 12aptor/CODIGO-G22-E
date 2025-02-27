ef03edef4fd1ccf53223e14fe351c1a73c428982
12aptor <paholo2054@gmail.com>
2025-02-26 21:37:38 -0500

    Django admin

--- semana05/django_intro/almacen/admin.py
+++ semana05/django_intro/almacen/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import CategoryModel, ProductModel
 
-# Register your models here.
+class ProductsInline(admin.TabularInline):
+    model = ProductModel
+    extra = 0
+    readonly_fields = ('name', 'price', 'status', 'category')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status')
+    search_fields = ('name',)
+    list_per_page = 10
+    inlines = [ProductsInline]
+
+admin.site.register(CategoryModel, CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'status', 'category')
+    search_fields = ('name', 'category__name')
+    list_per_page = 10
+
+admin.site.register(ProductModel, ProductAdmin)
--- semana05/django_intro/almacen/models.py
+++ semana05/django_intro/almacen/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class CategoryModel(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name='Nombre')
 
     STATUS_CHOICES = (
         ('ACTIVE', 'ACTIVE'),
@@ -10,26 +10,29 @@ class CategoryModel(models.Model):
         ('DELETED', 'DELETED'),
     )
 
-    status = models.CharField(choices=STATUS_CHOICES, max_length=10)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=10, verbose_name='Estado')
 
     def __str__(self):
         return self.name
     
     class Meta:
         db_table = 'categories'
+        verbose_name = 'Categoria de productos'
+        verbose_name_plural = 'Categorias de productos'
 
 class ProductModel(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=200)
-    price = models.FloatField()
-    status = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=200, verbose_name='Nombre')
+    price = models.FloatField(verbose_name='Precio')
+    status = models.BooleanField(default=True, verbose_name='Estado')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Creado en')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Actualizado en')
     category = models.ForeignKey(
         CategoryModel,
         on_delete=models.CASCADE,
         db_column='category_id',
-        related_name='products'
+        related_name='products',
+        verbose_name='Categoria'
     )
 
     def __str__(self):
@@ -37,3 +40,5 @@ class ProductModel(models.Model):
     
     class Meta:
         db_table = 'products'
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Productos'
