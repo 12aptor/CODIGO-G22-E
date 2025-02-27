9fae75961d30e573f3bfbba91d0e7e686781f3c5
12aptor <paholo2054@gmail.com>
2025-02-26 22:19:21 -0500

    Django views

--- semana05/django_intro/almacen/views.py
+++ semana05/django_intro/almacen/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import ProductModel
+import datetime
 
-# Create your views here.
+def index(request):
+    return HttpResponse('Hola Django 👻')
+
+def date(request):
+    now = datetime.datetime.now()
+    html = f'<html><body>Ahora es: {now}</body></html>'
+    return HttpResponse(html)
+
+def json(request):
+    data = {
+        'nombre': 'Pepito'
+    }
+    return JsonResponse(data)
+
+def product(request, id):
+    product = ProductModel.objects.get(id=id)
+    return render(
+        request,
+        'product.html',
+        context={
+            'name': product.name
+        }
+    )
