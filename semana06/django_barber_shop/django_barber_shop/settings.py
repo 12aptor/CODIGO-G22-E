e2d1153de86d33ef06fcf16336f706afb8ba4054
12aptor <paholo2054@gmail.com>
2025-02-28 22:50:07 -0500

    Servicios y transacciones

--- semana06/django_barber_shop/django_barber_shop/settings.py
+++ semana06/django_barber_shop/django_barber_shop/settings.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authentication',
+    'services',
+    'transactions',
 ]
 
 MIDDLEWARE = [
---
+++ semana06/django_barber_shop/transactions/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from authentication.models import UserModel
+from services.models import BarberModel, ServiceModel
+
+class CustomerModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=100, unique=True)
+    document_number = models.CharField(max_length=20, unique=True)
+    address = models.CharField(max_length=200)
+
+class AppointmentModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    appointiment_data = models.DateTimeField()
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(
+        UserModel,
+        on_delete=models.CASCADE,
+        related_name='appointments',
+        db_column='user_id'
+    )
+    barber = models.ForeignKey(
+        BarberModel,
+        on_delete=models.CASCADE,
+        related_name='appointments',
+        db_column='barber_id'
+    )
+    service = models.ForeignKey(
+        ServiceModel,
+        on_delete=models.CASCADE,
+        related_name='appointments',
+        db_column='service_id'
+    )
+    customer = models.ForeignKey(
+        CustomerModel,
+        on_delete=models.CASCADE,
+        related_name='appointments',
+        db_column='customer_id'
+    )
+
+class PaymentModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    amount = models.IntegerField()
+
+    PAYMENT_METHOD_CHOICES = (
+        ('CASH', 'CASH'),
+        ('CARD', 'CARD'),
+        ('YAPE', 'YAPE'),
+        ('PLIN', 'PLIN'),
+    )
+
+    payment_method = models.CharField(
+        choices=PAYMENT_METHOD_CHOICES,
+        max_length=10
+    )
+    payment_date = models.DateTimeField()
+    appointment = models.ForeignKey(
+        AppointmentModel,
+        on_delete=models.CASCADE,
+        related_name='payments',
+        db_column='appointment_id'
+    )
