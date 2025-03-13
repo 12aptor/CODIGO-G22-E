8106a9fca182551a94882afff3a7a76ab0be347e
12aptor <paholo2054@gmail.com>
2025-03-12 22:53:17 -0500

    Typescript

---
+++ semana08/typescript/variables.ts
@@ -0,0 +1,18 @@
+let fullName: string = "John";
+let age: number = 20;
+let alive: boolean = true;
+let height: number = 1.8;
+let emptyValue = null;
+let notAssigned = undefined;
+
+let mixed: string | number;
+mixed = "Veinte";
+
+let numbers: number[];
+numbers = [1, 4, 10]
+
+let letters: string[];
+letters = ["a", "b", "c"];
+
+let married: "YES" | "NO";
+married = "NO";
